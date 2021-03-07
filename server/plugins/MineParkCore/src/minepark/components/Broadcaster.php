980c0eb5673b57b5b627b0cdfdeace365f84be08
Kirill Poroh <procompteam@gmail.com>
2021-03-07 14:45:17 +0300

    #73 Broadcaster update

--- server/plugins/MineParkCore/src/minepark/components/Broadcaster.php
+++ server/plugins/MineParkCore/src/minepark/components/Broadcaster.php
@@ -4,33 +4,46 @@ namespace minepark\components;
 use minepark\components\base\Component;
 use minepark\Core;
 use minepark\defaults\Defaults;
-use minepark\Providers;
-use minepark\providers\LocalizationProvider;
 use minepark\utils\CallbackTask;
 
 class Broadcaster extends Component
 {
-    private $translations;
+    private $localizationKeys;
+
+    private $localizationKeysMaxIndex;
+
+    private $localizationKeysCurrentIndex;
 
     public function __construct()
     {
+        $this->localizationKeys = $this->getMessagesLocalizationKeys();
+        $this->localizationKeysMaxIndex = count($this->localizationKeys) - 1;
+        $this->localizationKeysCurrentIndex = 0;
+
         $this->initializeBroadcastTask();
     }
 
     public function broadcastMessage()
     {
-        $localizationKeys = $this->getMessagesLocalizationKeys();
-
-        $randomMessageKey = $localizationKeys[random_int(0, count($localizationKeys) - 1)];
-
         foreach ($this->getCore()->getServer()->getOnlinePlayers() as $player) {
-            $player->sendMessage($randomMessageKey);
+            $player->sendMessage($$this->localizationKeys[$this->localizationKeysCurrentIndex]);
+        }
+
+        if($this->localizationKeysCurrentIndex < $this->localizationKeysMaxIndex) {
+            $this->localizationKeysCurrentIndex++;
+        } else {
+            $this->localizationKeysCurrentIndex = 0;
         }
     }
 
     private function getCore() : Core
     {
         return Core::getActive();
+    }
+
+    private function initializeBroadcastTask()
+    {
+        $this->getCore()->getScheduler()->scheduleRepeatingTask(new CallbackTask([$this, "broadcastMessage"]), Defaults::AUTO_BROADCAST_TIMEOUT * 20);
     }
 
     private function getMessagesLocalizationKeys() : array
@@ -45,13 +58,28 @@ class Broadcaster extends Component
             "AutoMessage7",
             "AutoMessage8",
             "AutoMessage9",
-            "AutoMessage10"
+            "AutoMessage10",
+            "AutoMessage11",
+            "AutoMessage12",
+            "AutoMessage13",
+            "AutoMessage14",
+            "AutoMessage15",
+            "AutoMessage16",
+            "AutoMessage17",
+            "AutoMessage18",
+            "AutoMessage19",
+            "AutoMessage20",
+            "AutoMessage21",
+            "AutoMessage22",
+            "AutoMessage23",
+            "AutoMessage24",
+            "AutoMessage25",
+            "AutoMessage26",
+            "AutoMessage27",
+            "AutoMessage28",
+            "AutoMessage29",
+            "AutoMessage30"
         ];
-    }
-
-    private function initializeBroadcastTask()
-    {
-        $this->getCore()->getScheduler()->scheduleRepeatingTask(new CallbackTask([$this, "broadcastMessage"]), Defaults::AUTO_BROADCAST_TIMEOUT * 20);
     }
 }
 ?>
