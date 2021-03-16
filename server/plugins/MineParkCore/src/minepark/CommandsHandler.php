2137297a20ad679c725b518a70ee8fcd6aa3080a
Gewinum <beardatunnel@gmail.com>
2021-03-16 11:00:30 +0300

    #409 Optimize CommandsHandler and sort usings

--- server/plugins/MineParkCore/src/minepark/CommandsHandler.php
+++ server/plugins/MineParkCore/src/minepark/CommandsHandler.php
@@ -1,47 +1,49 @@
 <?php
 namespace minepark;
 
+// please, place usings better
 use pocketmine\event\Event;
+use minepark\defaults\Permissions;
+use minepark\common\player\MineParkPlayer;
+
+use minepark\commands\base\Command;
 use minepark\commands\DayCommand;
 use minepark\commands\PayCommand;
 use minepark\commands\BankCommand;
-use minepark\defaults\Permissions;
-use minepark\commands\base\Command;
 use minepark\commands\LevelCommand;
 use minepark\commands\MoneyCommand;
 use minepark\commands\NightCommand;
 use minepark\commands\CasinoCommand;
 use minepark\commands\DonateCommand;
 use minepark\commands\OnlineCommand;
-use minepark\commands\map\GPSCommand;
+use minepark\commands\GetOrganisationCommand;
+use minepark\commands\ResetPasswordCommand;
 use minepark\commands\JailExitCommand;
 use minepark\commands\PassportCommand;
 use minepark\commands\AnimationCommand;
 use minepark\commands\GetSellerCommand;
-use minepark\commands\phone\SmsCommand;
 use minepark\commands\TransportCommand;
 use minepark\commands\phone\CallCommand;
+use minepark\commands\phone\SmsCommand;
 use minepark\commands\admin\AdminCommand;
+use minepark\commands\map\AddPointCommand;
 use minepark\commands\map\GPSNearCommand;
 use minepark\commands\map\ToPointCommand;
-use minepark\commands\roleplay\DoCommand;
-use minepark\commands\roleplay\MeCommand;
-use minepark\commands\map\AddPointCommand;
-use minepark\commands\report\CloseCommand;
-use minepark\commands\report\ReplyCommand;
-use minepark\commands\roleplay\TryCommand;
-use minepark\common\player\MineParkPlayer;
-use minepark\commands\report\ReportCommand;
-use minepark\commands\ResetPasswordCommand;
-use minepark\commands\roleplay\ShoutCommand;
-use minepark\commands\workers\PutBoxCommand;
-use minepark\commands\GetOrganisationCommand;
+use minepark\commands\map\GPSCommand;
 use minepark\commands\map\RemovePointCommand;
 use minepark\commands\map\ToNearPointCommand;
+use minepark\commands\report\CloseCommand;
+use minepark\commands\report\ReplyCommand;
+use minepark\commands\report\ReportCommand;
+use minepark\commands\roleplay\ShoutCommand;
+use minepark\commands\roleplay\WhisperCommand;
+use minepark\commands\roleplay\DoCommand;
+use minepark\commands\roleplay\MeCommand;
+use minepark\commands\roleplay\TryCommand;
+use minepark\commands\workers\PutBoxCommand;
 use minepark\commands\workers\GetFarmCommand;
 use minepark\commands\workers\PutFarmCommand;
 use minepark\commands\workers\TakeBoxCommand;
-use minepark\commands\roleplay\WhisperCommand;
 
 class CommandsHandler
 {
@@ -97,19 +99,22 @@ class CommandsHandler
 	
 	public function execute(MineParkPlayer $player, string $rawCommand, Event $event = null)
 	{
-		if($rawCommand[0] == "/") {
-			$rawCommand = substr($rawCommand, 1);
-		}
-
-		$arguments = explode(' ', $rawCommand);
-		$command = $this->getCommand($arguments[0]);
-		$arguments = array_slice($arguments, 1);
-
-		if($command === null) {
+		if ($rawCommand[0] !== "/") {
 			return;
 		}
 
-		if(!$this->hasPermissions($player, $command)) {
+		$rawCommand = substr($rawCommand, 1);
+
+		$arguments = explode(' ', $rawCommand);
+		$command = $this->getCommand($arguments[0]);
+
+		if ($command === null) {
+			return;
+		}
+
+		$arguments = array_slice($arguments, 1);
+
+		if (!$this->hasPermissions($player, $command)) {
 			$player->sendMessage("§cУ вас нет прав на эту команду :(");
 			$player->sendMessage("§6Возможно она станет доступна после покупки: /donate");
 
@@ -125,11 +130,9 @@ class CommandsHandler
 
 	private function getCommand(string $commandName) : ?Command
 	{
-		foreach($this->commands as $command) {
-			foreach($command->getCommand() as $currentCommandName) {
-				if($currentCommandName == $commandName) {
-					return $command;
-				}
+		foreach ($this->commands as $command) {
+			if (in_array($commandName, $command->getCommand())) {
+				return $command;
 			}
 		}
 
@@ -140,16 +143,16 @@ class CommandsHandler
 	{
 		$permissions = $command->getPermissions();
 
-		if(in_array(Permissions::ANYBODY, $permissions)) {
+		if (in_array(Permissions::ANYBODY, $permissions)) {
 			return true;
 		}
 
-		if(in_array(Permissions::OPERATOR, $permissions) and $player->isOp()) {
+		if (in_array(Permissions::OPERATOR, $permissions) and $player->isOp()) {
 			return true;
 		}
 
-		foreach($permissions as $permission) {
-			if($player->hasPermission($permission)) {
+		foreach ($permissions as $permission) {
+			if ($player->hasPermission($permission)) {
 				return true;
 			}
 		}
