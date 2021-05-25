bd6f0f5e1f1a956f8175aa2c58a4ee34de572950
Gaprix <gaprix.dev@outlook.com>
2021-05-25 23:00:18 +0300

    Change ClearLagg message

--- server/plugins/MineParkCore/src/minepark/components/map/ClearLagg.php
+++ server/plugins/MineParkCore/src/minepark/components/map/ClearLagg.php
@@ -30,7 +30,7 @@ class ClearLagg extends Component
                 }
 
                 if ($entity instanceof MineParkPlayer) {
-                    $entity->sendTip("§dКоммунальные службы очистили город от мусора!");
+                    $entity->sendTip("§dКоммунальные службы очистили среду от мусора!");
                 }
 
             }
