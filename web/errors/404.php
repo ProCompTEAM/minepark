aa73cf35369e989990fdc557fe4178aeb031b873
Кирилл Тарасевич <opl.opu@mail.ru>
2021-06-03 21:00:15 +0300

    Add 404 page

---
+++ web/errors/404.php
@@ -0,0 +1,27 @@
+<?php 
+	header("HTTP/1.1 404 Not Found");
+?>
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="/css/404.css">
+
+	<title>Cтраница не найдена</title>
+</head>
+<body>
+	<header class="header">
+		<div class="wrapper">
+			<div class="img_error"></div>
+			<div class="information">
+				<h2 class="information__H2">К сожилению данный путь не найден:(</h2>
+				<p class="information__P">вскорее всего данный путь перенесён или во все не существовал.</p>
+			</div>
+			<div class="button">
+				<a href="/" class="but"></a>
+			</div>
+		</div>
+	</header>
+</body>
+</html>
