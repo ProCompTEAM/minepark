f0eed8483ad77b0b413e8db76f154deaca9ba06b
Kirill Poroh <procompteam@gmail.com>
2021-06-19 00:42:48 +0300

    Add '/t station <number>' command logic

--- server/plugins/MineParkCore/src/minepark/commands/TransportCommand.php
+++ server/plugins/MineParkCore/src/minepark/commands/TransportCommand.php
@@ -7,6 +7,7 @@ use minepark\commands\base\Command;
 use minepark\common\player\MineParkPlayer;
 use minepark\Components;
 use minepark\components\vehicles\Vehicles;
+use minepark\defaults\Sounds;
 
 class TransportCommand extends Command
 {
@@ -29,32 +30,66 @@ class TransportCommand extends Command
     public function getPermissions() : array
     {
         return [
-            Permissions::OPERATOR,
-            Permissions::ADMINISTRATOR
+            Permissions::ANYBODY
         ];
     }
 
     public function execute(MineParkPlayer $player, array $args = array(), Event $event = null)
     {
         if(self::argumentsNo($args)) {
-            return $player->sendMessage("Неправильное использование команды. /t spawn <машина>");
+            $player->sendMessage("Неправильное использование команды. /t spawn <машина>");
+
+            return;
         }
+
+        $subCommand = $args[0];
         
-        if ($args[0] === "spawn") {
+        if ($subCommand == "spawn") {
+            if (!$player->isAdministrator()) {
+                $player->sendMessage("Необходимо запросить доступ для выполнения этой команды!");
+
+                return;
+            }
+
             if (!self::argumentsMin(2, $args)) {
-                return $player->sendMessage("Неправильное использование команды. /t spawn <машина>");
+                $player->sendMessage("Неправильное использование команды. /t spawn <машина>");
+
+                return;
             }
 
             if (!$this->spawnCar($player, $args[1])) {
-                return $player->sendMessage("Неверное название модели машины!");
+                $player->sendMessage("Неверное название модели машины!");
+
+                return;
             }
 
             $player->sendMessage("Машина успешно создана.");
+        } elseif($subCommand == "station") {
+            //TODO: Add check: is @driver in @vehicle only
+
+            if (!isset($args[1])) {
+                $player->sendMessage("Неправильное использование команды. /t station <порядковый номер>");
+
+                return;
+            }
+
+            $this->broadcastTrainStationSound($player, Sounds::STATION, 0);
+            $this->broadcastTrainStationSound($player, $this->getSoundForStationNumber($args[1]), 2);
         }
     }
 
     private function spawnCar(MineParkPlayer $player, string $model) : bool
     {
         return $this->vehicles->createVehicle($model, $player->getLevel(), $player->asVector3(), $player->getYaw());
+    }
+
+    private function broadcastTrainStationSound(MineParkPlayer $driver, string $sound, int $delay)
+    {
+        
+    }
+
+    private function getSoundForStationNumber(string $stationCode) : string
+    {
+        return Sounds::STATION + $stationCode;
     }
 }
