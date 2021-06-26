845f40a9e314992d1db93cef4701aadce7d2ae8b
Kirill Poroh <procompteam@gmail.com>
2021-06-26 11:31:12 +0300

    Update textures

--- server/resource_packs/compress-textures-zip.ps1
+++ server/resource_packs/compress-textures-zip.ps1
@@ -1,4 +1,23 @@
+#remove previous zip file
 rm textures.zip
 
-Add-Type -Assembly "System.IO.Compression.FileSystem" ;
-[System.IO.Compression.ZipFile]::CreateFromDirectory("textures_src", "textures.zip") ;
+
+#update pack manifest 
+$path = "textures_src\manifest.json";
+
+$year = [int] (Get-Date -Format yyyy);
+$month = [int] (Get-Date -Format MM);
+$day = [int] (Get-Date -Format dd);
+
+$manifest = Get-Content $path -raw | ConvertFrom-Json;
+$manifest.header.uuid = [GUID]::NewGuid();
+$manifest.header.version = ($year, $month, $day);
+$manifest.modules[0].uuid = [GUID]::NewGuid();
+$manifest.modules[0].version = ($year, $month, $day);
+$manifest.update;
+$manifest | ConvertTo-Json -depth 32| set-content $path;
+
+
+#create zip file
+Add-Type -Assembly "System.IO.Compression.FileSystem";
+[System.IO.Compression.ZipFile]::CreateFromDirectory("textures_src", "textures.zip");
