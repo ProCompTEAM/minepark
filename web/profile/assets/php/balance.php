9f96c986002b40b6244c12c64b5bb71b495d7bf8
kirill22890 <opl.opu@mail.ru>
2021-09-07 18:58:54 +0300

    Replacing $ with rub

--- web/profile/assets/php/balance.php
+++ web/profile/assets/php/balance.php
@@ -1,19 +1,19 @@
 <?php 
-    $balance = 1000060;
+    $balance = 1000000;
 
-    if($balance > 1000000) {
+    if($balance >= 1000000) {
         $balance_sum = $balance / 1000000;
         $remains = $balance % 1000000;
         $balance = round($balance_sum, 1);
         $balance = floor($balance);
         if($remains == 0) {
-            $balance_str = $balance . ' млн. ' . '$';
+            $balance_str = $balance . ' млн. ' . ' руб.';
         } else {
-            $balance_str = $balance . ' млн. ' . $remains . ' $';
+            $balance_str = $balance . ' млн. ' . $remains . ' руб.';
         }
     }
     elseif ($balance < 1000) {
-        $balance_str = $balance . '$';
+        $balance_str = $balance . 'руб';
     }
     elseif ($balance < 1000000) {
         $balance_sum = $balance / 1000;
@@ -21,9 +21,9 @@
         $balance = round($balance_sum, 1);
         $balance = floor($balance);
         if($remains == 0){
-            $balance_str = $balance . ' тыс. ' . ' $';
+            $balance_str = $balance . ' тыс. ' . ' руб.';
         } else {
-            $balance_str = $balance . ' тыс. ' . $remains . ' $';
+            $balance_str = $balance . ' тыс. ' . $remains . ' руб.';
         }
     }
 ?>
