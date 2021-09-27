fbf50fd733068e6b3bff9c4d62cc29902140dd9f
FirzMob <firzmob@gmail.com>
2021-09-27 19:48:23 +0300

    #517 Add phone sounds & fix keys of localization

--- server/plugins/MineParkCore/src/minepark/defaults/Sounds.php
+++ server/plugins/MineParkCore/src/minepark/defaults/Sounds.php
@@ -16,4 +16,21 @@ class Sounds
     public const SIREN_SOUND = "minepark.sound5";
 
     public const TRAIN_STATION = "train.station";
+
+    public const PHONE_INCOMING_CALL = "phone.loud1";
+
+    public const PHONE_UNCONNECT = "phone.loud2";
+
+    public const PHONE_OUTCOMMING_CALL = "phone.loud3";
+
+    public const PHONE_UNDIALING = "phone.loud4";
+
+    public const PHONE_CHECK_NUM = "phone.loud5";
+
+    public const PHONE_UNAVAILABLE = "phone.loud6";
+
+    public const PHONE_SMS = "phone.loud7";
+
+    public const PHONE_LOST_CONNECT = "phone.loud8";
+
 }
